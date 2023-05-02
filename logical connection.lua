b2cb5d582e8e3a3d247287e4fd31df598ec8b8b9
OuiSom89 <107348304+OuiSom89@users.noreply.github.com>
2023-05-02 20:39:52 +0200

    Update logical connection.lua

--- logical connection.lua
+++ logical connection.lua
@@ -18,16 +18,8 @@ ClientY = ClientScreenGui.y/OuiSomScreenGui.y
 local ImageLabel = Instance.new("ImageLabel",ScreenGUII)
 ImageLabel.Size = UDim2.new(1, 0, 1.0356, 0)
 ImageLabel.BackgroundColor3 = Color3.new(0,0,0)
-ImageLabel.Position = UDim2.new(0, 0, -0.0356, 0)
+ImageLabel.Position = UDim2.new(0, 0, -1.0750, 0)
 ImageLabel.Image = "rbxasset://textures/AvatarEditorImages/AvatarEditor.png"
-
-local function onHover(Fname)
-    Fname.Background.StateOverlay.ImageTransparency = 0.9
-end
-
-local function onLeave(Fname)
-    Fname.Background.StateOverlay.ImageTransparency = 1
-end
 
 local SettingsModule = Instance.new("Frame", game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame)
 SettingsModule.Size = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.MenuIcon.Size
@@ -35,13 +27,18 @@ SettingsModule.Name = "SettingsModule"
 SettingsModule.BackgroundTransparency = 1
 SettingsModule.LayoutOrder = 2
 
-local clone = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.MenuIcon.Background:clone()
-clone.Parent = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.SettingsModule
-clone.FlashingDot:Destroy()
-clone.Icon.Image = "rbxasset://textures/ui/Settings/MenuBarIcons/GameSettingsTab.png"
+local Background = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.MenuIcon.Background:clone()
+Background.Parent = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.SettingsModule
+Background.FlashingDot:Destroy()
+Background.Icon.Image = "rbxasset://textures/ui/Settings/MenuBarIcons/GameSettingsTab.png"
 
-SettingsModule.MouseEnter:Connect(onHover(SettingsModule))
-SettingsModule.MouseLeave:Connect(onLeave(SettingsModule))
+SettingsModule.MouseEnter:Connect(function ()
+    SettingsModule.Background.StateOverlay.ImageTransparency = 0.9
+end)
+
+SettingsModule.MouseLeave:Connect(function ()
+    SettingsModule.Background.StateOverlay.ImageTransparency = 1
+end)
 
 local ScriptWritter = Instance.new("Frame", game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame)
 ScriptWritter.Size = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.MenuIcon.Size
@@ -49,13 +46,18 @@ ScriptWritter.Name = "ScriptWritter"
 ScriptWritter.BackgroundTransparency = 1
 ScriptWritter.LayoutOrder = 2
 
-clone = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.MenuIcon.Background:clone()
-clone.Parent = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.ScriptWritter
-clone.FlashingDot:Destroy()
-clone.Icon.Image = "rbxasset://"..getsynasset("ScriptTool/Pictures/ScriptIcon.png")
+Background = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.MenuIcon.Background:clone()
+Background.Parent = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.ScriptWritter
+Background.FlashingDot:Destroy()
+Background.Icon.Image = "rbxasset://"..getsynasset("ScriptTool/Pictures/ScriptIcon.png")
 
-ScriptWritter.MouseEnter:Connect(onHover(ScriptWritter))
-ScriptWritter.MouseLeave:Connect(onLeave(ScriptWritter))
+ScriptWritter.MouseEnter:Connect(function ()
+    ScriptWritter.Background.StateOverlay.ImageTransparency = 0.9
+end)
+
+ScriptWritter.MouseLeave:Connect(function ()
+    ScriptWritter.Background.StateOverlay.ImageTransparency = 1
+end)
 
 local OpenAIChatGPT = Instance.new("Frame", game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame)
 OpenAIChatGPT.Size = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.MenuIcon.Size
@@ -63,13 +65,18 @@ OpenAIChatGPT.Name = "OpenAIChatGPT"
 OpenAIChatGPT.BackgroundTransparency = 1
 OpenAIChatGPT.LayoutOrder = 2
 
-clone = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.MenuIcon.Background:clone()
-clone.Parent = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.OpenAIChatGPT
-clone.FlashingDot:Destroy()
-clone.Icon.Image = "rbxasset://"..getsynasset("ScriptTool/Pictures/OpenAIIcon.png")
+Background = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.MenuIcon.Background:clone()
+Background.Parent = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.OpenAIChatGPT
+Background.FlashingDot:Destroy()
+Background.Icon.Image = "rbxasset://"..getsynasset("ScriptTool/Pictures/OpenAIIcon.png")
 
-OpenAIChatGPT.MouseEnter:Connect(onHover(OpenAIChatGPT))
-OpenAIChatGPT.MouseLeave:Connect(onLeave(OpenAIChatGPT))
+OpenAIChatGPT.MouseEnter:Connect(function ()
+    OpenAIChatGPT.Background.StateOverlay.ImageTransparency = 0.9
+end)
+
+OpenAIChatGPT.MouseLeave:Connect(function ()
+    OpenAIChatGPT.Background.StateOverlay.ImageTransparency = 1
+end)
 
 local MenuSwap = Instance.new("Frame", game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame)
 MenuSwap.Size = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.MenuIcon.Size
@@ -77,10 +84,32 @@ MenuSwap.Name = "MenuSwap"
 MenuSwap.BackgroundTransparency = 1
 MenuSwap.LayoutOrder = 0
 
-clone = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.MenuIcon.Background:clone()
-clone.Parent = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.MenuSwap
-clone.FlashingDot:Destroy()
-clone.Icon.Image = "rbxasset://textures/WindControl/ArrowDown.png"
+Background = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.MenuIcon.Background:clone()
+Background.Parent = game:GetService("CoreGui").TopBarApp.TopBarFrame.LeftFrame.MenuSwap
+Background.FlashingDot:Destroy()
+Background.Icon.Image = "rbxasset://textures/WindControl/ArrowDown.png"
 
-MenuSwap.MouseEnter:Connect(onHover(MenuSwap))
-MenuSwap.MouseLeave:Connect(onLeave(MenuSwap))
+local hovering = false
+
+MenuSwap.MouseEnter:Connect(function ()
+hovering = true
+    MenuSwap.Background.StateOverlay.ImageTransparency = 0.9
+end)
+
+MenuSwap.MouseLeave:Connect(function ()
+hovering = false
+    MenuSwap.Background.StateOverlay.ImageTransparency = 1
+end)
+
+game:GetService('UserInputService').InputBegan:connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 and hovering then
+        print("ge")
+        local rotationTween = game:GetService("TweenService"):Create(MenuSwap.Background.Icon, TweenInfo.new(0.5, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {Rotation = MenuSwap.Background.Icon.Rotation + 180})rotationTween:Play()
+        print(tonumber(string.sub(tostring(ImageLabel.Position.Y.Scale),1,5)))
+        if tonumber(string.sub(tostring(ImageLabel.Position.Y.Scale),1,5)) == -0.03 then
+            ImageLabel:TweenPosition(UDim2.new(0,0,-1.0750,0))
+        else
+            ImageLabel:TweenPosition(UDim2.new(0,0,-0.0356,0))
+        end
+    end
+end)
